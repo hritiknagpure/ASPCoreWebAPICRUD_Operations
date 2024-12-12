0878dd74a2261fd57bf89e4ef3a070c01b062272
hriti <hriti@HRITIKNAGPURE>
2024-12-12 10:19:20 +0530

    Add project files.

---
+++ ASPCoreWebAPICRUD/ModelsModels/MyDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASPCoreWebAPICRUD.ModelsModels
+{
+    public partial class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+        }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=HRITIKNAGPURE\\SQLEXPRESS;Database=MyDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("students");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Fathername)
+                    .HasMaxLength(100)
+                    .HasColumnName("fathername");
+
+                entity.Property(e => e.Standard)
+                    .HasMaxLength(50)
+                    .HasColumnName("standard");
+
+                entity.Property(e => e.Studentname)
+                    .HasMaxLength(100)
+                    .HasColumnName("studentname");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ASPCoreWebAPICRUD/ModelsModels/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPCoreWebAPICRUD.ModelsModels
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Studentname { get; set; } = null!;
+        public int? Age { get; set; }
+        public string? Standard { get; set; }
+        public string? Fathername { get; set; }
+        //public string Name { get; internal set; }
+    }
+}
